376ae0cf2b02c60cc443b1e6a5b9a7ec208f98ba
Arek <arek.stachyra41@gmail.com>
2022-08-01 12:23:03 +0200

    implement initial functionaliy to add book to DOM

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ let myLibrary = [];
 bookAddForm.addEventListener('submit', (e) => {
     e.preventDefault();
     addBookToLibrary();
-    displayBook();
+    createBookCard(myLibrary[0]);
+    // displayBook();
 })
 
 // BOOK OBJECT AND METHOD CONSTRUCTOR
@@ -34,9 +35,45 @@ function addBookToLibrary() {
 }
 
 // maybe make this function accept props when called
-function createBookCard() {
+function createBookCard(book) {
     const bookCard = document.createElement('div');
+    bookCardContainer.appendChild(bookCard);
     bookCard.classList.add('book-card');
+
+    const bookInfo = document.createElement('div');
+    bookCard.appendChild(bookInfo);
+    bookInfo.classList.add('book-info');
+    const h2 = document.createElement('h2');
+    h2.textContent = book.title;
+    const p1 = document.createElement('p'); 
+    p1.textContent = book.author;
+    const p2 = document.createElement('p'); 
+    p2.textContent = book.pages;
+    bookInfo.appendChild(h2);
+    bookInfo.appendChild(p1);
+    bookInfo.appendChild(p2);
+
+    const bookEdit = document.createElement('div');
+    bookEdit.classList.add('book-edit');
+    const readStatus = document.createElement('div');
+    readStatus.classList.add('read-status');
+    const p3 = document.createElement('p'); 
+    p3.textContent = 'Read:';
+    const label = document.createElement('label');
+    label.classList.add('switch');
+    const input = document.createElement('input');
+    input.setAttribute('type', 'checkbox');
+    const span = document.createElement('span');
+    span.classList.add('slider', 'round');
+    label.appendChild(input);
+    label.appendChild(span);
+    readStatus.appendChild(p3);
+    readStatus.appendChild(label);
+    const img = document.createElement('img');
+    img.setAttribute('src', 'images/delete.svg');
+    bookEdit.appendChild(readStatus);
+    bookEdit.appendChild(img);
+    bookCard.appendChild(bookEdit);
 }
 
 function displayBook() {
